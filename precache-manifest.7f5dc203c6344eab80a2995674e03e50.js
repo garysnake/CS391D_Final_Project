80dcd85b5b180ed35a065a6300d8922a93d501fe
Gary Zhong <garysnake@Garys-MBP.attlocal.net>
2020-03-12 13:59:18 -0500

    Updates

---
+++ precache-manifest.7f5dc203c6344eab80a2995674e03e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43fbecdee7656e73ee7e52abe69a9ae",
+    "url": "/CS391D_Final_Project/index.html"
+  },
+  {
+    "revision": "89c2f27d7e4524bbd0a5",
+    "url": "/CS391D_Final_Project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f1004d121328e610fe4",
+    "url": "/CS391D_Final_Project/static/js/2.96c38add.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/CS391D_Final_Project/static/js/2.96c38add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c2f27d7e4524bbd0a5",
+    "url": "/CS391D_Final_Project/static/js/main.f5c49be4.chunk.js"
+  },
+  {
+    "revision": "69ae18745485aed508f8",
+    "url": "/CS391D_Final_Project/static/js/runtime-main.94c49e26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CS391D_Final_Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
